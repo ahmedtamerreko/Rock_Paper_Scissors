f79d6e206a6caa77c61c63b2561b765c0611db17
ahmedtamer <rekotamer4@gamil.com>
2024-07-03 14:06:14 +0300

    making the game as UI

--- rock_paper_scissors.js
+++ rock_paper_scissors.js
@@ -1,10 +1,3 @@
-var humanScore = 0;
-var computerScore = 0;
-
-const score = (computerChoice) => {
-    console.log(`computer chooses ${computerChoice}\nComputer score is now ${computerScore}\nYour score is now ${humanScore}\n`)
-}
-
 function getComputerChoice() {
     const comp = Math.floor(Math.random() * 3);
     if (comp === 0) {
@@ -16,35 +9,79 @@ function getComputerChoice() {
     }
 }
 
-function getHumanChoice() {
-    return prompt("Please enter rock, paper, or scissors \n note: use console to see the game").toLowerCase();
-}
-
-function playRound(humanChoice, computerChoice) {
+function playingRound(humanChoice, computerChoice) {
     if (humanChoice === computerChoice) {
-        console.log("Tie");
+        div.textContent = "Tie";
     } else if (
         (humanChoice === "rock" && computerChoice === "scissors") ||
         (humanChoice === "paper" && computerChoice === "rock") ||
         (humanChoice === "scissors" && computerChoice === "paper")
     ) {
-        console.log("You won this round");
+        div.textContent = "You won this round";
         humanScore++;
     } else {
-        console.log("Computer won this round");
+        div.textContent = "Computer won this round";
         computerScore++;
     }
-    score(computerChoice);
+    ShowingTheScore(computerChoice);
 }
 
-while (computerScore !== 3 && humanScore !== 3) {
-    var humanChoice = getHumanChoice();
+function ShowingTheScore(computerChoice) {
+    if (computerScore === 3) {
+        div.textContent = "Computer wins";
+        div.style.color = "purple"
+        DisablingButtons();
+    }
+    else if (humanScore === 3) {
+        div.textContent = "Congrats,You Win The Game"
+        div.style.color = "red";
+        DisablingButtons();
+    }
+    else {
+        div.innerHTML = `<p><br>Computer chooses ${computerChoice}</p>`
+        div.innerHTML += `<p style="color:purple">Computer score now is ${computerScore}</p>`
+        div.innerHTML += `<p style="color:red">Your score now is ${humanScore}</p>`;
+    }
+}
+
+function DisablingButtons() {
+    btnRock.disabled = true;
+    btnPaper.disabled = true;
+    btnScissors.disabled = true;
+}
+
+var humanScore = 0;
+var computerScore = 0;
+
+const div = document.querySelector("div");
+const btnRock = document.getElementById("rock");
+const btnPaper = document.getElementById("paper");
+const btnScissors = document.getElementById("scissors");
+
+btnRock.addEventListener("click", () => {
+    btnRock.style.background = "red";
+    btnPaper.style.background = "white";
+    btnScissors.style.background = "white";
     var computerChoice = getComputerChoice();
-    playRound(humanChoice, computerChoice);
-}
+    playingRound("rock", computerChoice);
+});
 
-if (computerScore === 3) {
-    console.log("Computer wins the game!");
-} else {
-    console.log("You win the game!");
-}
+btnPaper.addEventListener("click", () => {
+    btnRock.style.background = "white";
+    btnPaper.style.background = "red";
+    btnScissors.style.background = "white";
+    var computerChoice = getComputerChoice();
+    playingRound("paper", computerChoice);
+});
+
+btnScissors.addEventListener("click", () => {
+    btnRock.style.background = "white";
+    btnPaper.style.background = "white";
+    btnScissors.style.background = "red";
+    var computerChoice = getComputerChoice();
+    playingRound("scissors", computerChoice);
+});
+
+
+
+
